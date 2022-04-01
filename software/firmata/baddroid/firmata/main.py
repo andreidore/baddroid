3178f559b8dcd4cc4bc6eb542a85485b91ec2a9e
Andrei Dore <andrei.dore@gmail.com>
2022-04-01 23:03:09 +0300

    Change layout

---
+++ software/firmata/baddroid/firmata/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Start firmata.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ software/video/baddroid/video/main.py
@@ -0,0 +1,9 @@
+import os
+
+
+def main():
+    print("Start video node")
+
+
+if __name__ == "__main__":
+    main()
